a058bc52a76a10d0c5ce32edea51e4e513a60f36
Anil Nair <anilnairxyz@gmail.com>
2018-04-21 13:23:26 +0100

    Change loss model to market risk

--- loss_model.py
+++ loss_model.py
@@ -3,48 +3,43 @@ import math
 from scipy import stats
 
 
-def _sample_loss(landfall_rate, mean, stddev):
+def _sample_loss(crash_rate, mean, stddev):
     '''
     Generate a loss using a Poisson-LogNormal assumption.
     '''
-    landfalls = stats.poisson.rvs(landfall_rate)
+    crashes = stats.poisson.rvs(crash_rate)
     loss = 0
-    for landfall in range(landfalls):
+    for crash in range(crashes):
         loss += stats.lognorm.rvs(stddev, math.exp(mean))
     return loss
 
 
-def _sample_us_hurricane_annual_loss(florida_landfall_rate, florida_mean,
-                                     florida_stddev, gulf_landfall_rate,
-                                     gulf_mean, gulf_stddev):
+def _sample_market_crash_annual(macroevent_rate, macroevent_mean, macroevent_stddev,
+                                selfevent_rate, selfevent_mean, selfevent_stddev):
     '''
-    Sample the annual economic loss due to US hurricanes using a
-    very simple model of Poisson-LogNormals for Florida and Gulf landfalls.
+    Sample the annual loss due to market events using a
+    very simple model of Poisson-LogNormals for Macro and Company Specific events.
     '''
-    florida_loss = _sample_loss(florida_landfall_rate, florida_mean,
-                                florida_stddev)
-    gulf_loss = _sample_loss(gulf_landfall_rate, gulf_mean, gulf_stddev)
-    return florida_loss + gulf_loss
+    macro_loss = _sample_loss(macroevent_rate, macroevent_mean, macroevent_stddev)
+    self_loss = _sample_loss(selfevent_rate, selfevent_mean, selfevent_stddev)
+    return macro_loss + self_loss
 
 
-def estimate_us_hurricane_annual_loss(num_monte_carlo_samples,
-                                      florida_landfall_rate, florida_mean,
-                                      florida_stddev, gulf_landfall_rate,
-                                      gulf_mean, gulf_stddev):
+def estimate_annual_loss(num_samples, macroevent_rate, macroevent_mean,
+                         macroevent_stddev, selfevent_rate,
+                         selfevent_mean, selfevent_stddev):
     '''
-    Generate a Monte-Carlo estimate of the annual economic loss due to
-    US hurricanes using a very simple model of Poisson-LogNormals
-    for Floria and Gulf landfalls.
+    Generate a Monte-Carlo estimate of the annual market loss due to
+    Macroeconomic events using a very simple model of Poisson-LogNormals
+    for Marco and COmpany Specific events.
     '''
     total_loss = 0
-    for sample in range(num_monte_carlo_samples):
-        total_loss += _sample_us_hurricane_annual_loss(florida_landfall_rate,
-                                                       florida_mean,
-                                                       florida_stddev,
-                                                       gulf_landfall_rate,
-                                                       gulf_mean, gulf_stddev)
-    return total_loss / num_monte_carlo_samples
+    for sample in range(num_samples):
+        total_loss += _sample_market_crash_annual(macroevent_rate, macroevent_mean,
+                                                  macroevent_stddev, selfevent_rate,
+                                                  selfevent_mean, selfevent_stddev)
+    return total_loss / num_samples
 
 
 if __name__ == '__main__':
-    print (estimate_us_hurricane_annual_loss(500, 10, 1, 1.5, 20, 2, 1.5))
+    print (estimate_annual_loss(500, 10, 1, 1.5, 20, 2, 1.5))
